e388111880b294ce3df16e5f1aa437690cff3b0f
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-04-15 22:41:41 +0200

    feat(http): add http basic auth

--- httpclients.go
+++ httpclients.go
@@ -5,10 +5,39 @@ import (
 	"net/http"
 )
 
-func NewHTTPClient(authType string) (*http.Client, error) {
+type HTTPBasicAuthConfig struct {
+	username string
+	password string
+}
+
+type HTTPBasicAuthRoundTripper struct {
+	rt     http.RoundTripper
+	config HTTPBasicAuthConfig
+}
+
+func (brt HTTPBasicAuthRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.SetBasicAuth(brt.config.username, brt.config.password)
+
+	return brt.rt.RoundTrip(req)
+}
+
+func NewBasicAuthClient(config HTTPBasicAuthConfig) *http.Client {
+	roundTripper := HTTPBasicAuthRoundTripper{
+		config: config,
+		rt:     http.DefaultTransport,
+	}
+
+	return &http.Client{
+		Transport: roundTripper,
+	}
+}
+
+func NewHTTPClient(authType string, authConfig interface{}) (*http.Client, error) {
 	switch authType {
 	case "no-auth":
 		return &http.Client{}, nil
+	case "basic-auth":
+		return NewBasicAuthClient(authConfig.(HTTPBasicAuthConfig)), nil
 	default:
 		return nil, fmt.Errorf("%s is not implemented", authType)
 	}
--- httpclients_test.go
+++ httpclients_test.go
@@ -1,12 +1,19 @@
 package busylight_sync
 
 import (
+	"encoding/base64"
 	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/h2non/gock.v1"
 )
+
+// Function from:  https://github.com/golang/go/blob/master/src/net/http/client.go#L417
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
 
 func TestHTTPClientNotImplemented(t *testing.T) {
 	assert := assert.New(t)
@@ -34,4 +41,54 @@ func TestHTTPClientNoAuth(t *testing.T) {
 	resp, err := sut.Get(url)
 	assert.NoError(err)
 	assert.Equal(200, resp.StatusCode)
+}
+
+func TestHTTPClientBasicAuth(t *testing.T) {
+	testCases := []struct {
+		desc       string
+		authConfig HTTPBasicAuthConfig
+		statusCode int
+	}{
+		{
+			desc:       "OK",
+			authConfig: HTTPBasicAuthConfig{username: "foobar", password: "spameggs"},
+			statusCode: 200,
+		},
+		{
+			desc:       "OK no password",
+			authConfig: HTTPBasicAuthConfig{username: "foobar", password: ""},
+			statusCode: 200,
+		},
+		{
+			desc:       "OK no username",
+			authConfig: HTTPBasicAuthConfig{username: "", password: "spameggs"},
+			statusCode: 200,
+		},
+		{
+			desc:       "OK no username and password",
+			authConfig: HTTPBasicAuthConfig{username: "", password: ""},
+			statusCode: 200,
+		},
+	}
+	for _, tC := range testCases {
+		authType := "basic-auth"
+		url := "https://foo.bar"
+
+		t.Run(tC.desc, func(t *testing.T) {
+			assert := assert.New(t)
+
+			defer gock.Off()
+			gock.New(url).
+				MatchHeader(
+					"Authorization",
+					"Basic "+basicAuth(tC.authConfig.username, tC.authConfig.password),
+				).
+				Reply(tC.statusCode)
+
+			sut, err := NewHTTPClient(authType, tC.authConfig)
+			assert.NoError(err)
+
+			got, err := sut.Get(url)
+		})
+	}
 }
