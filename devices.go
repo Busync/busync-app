7cbebf75ce9bc2f38c6705171bdf0dad48d04186
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-04-16 16:24:02 +0200

    feat(devices): add FakeDevice.Off() method

--- devices.go
+++ devices.go
@@ -1,7 +1,12 @@
 package busylight_sync
 
+var (
+	OffColor RGBColor = RGBColor{red: 0, green: 0, blue: 0}
+)
+
 type Device interface {
 	SetStaticColor(color RGBColor)
+	Off()
 }
 
 type FakeDevice struct {
@@ -10,4 +15,8 @@ type FakeDevice struct {
 
 func (f *FakeDevice) SetStaticColor(color RGBColor) {
 	f.color = color
+}
+
+func (f *FakeDevice) Off() {
+	f.color = OffColor
 }
--- devices_test.go
+++ devices_test.go
@@ -12,8 +12,8 @@ func TestFakeDeviceSetStaticColor(t *testing.T) {
 		color RGBColor
 	}{
 		{
-			desc:  "led off",
-			color: RGBColor{red: 0, green: 0, blue: 0},
+			desc:     "led off",
+			color: OffColor,
 		},
 		{
 			desc:  "white",
@@ -28,6 +28,33 @@ func TestFakeDeviceSetStaticColor(t *testing.T) {
 			sut.SetStaticColor(tC.color)
 
 			assert.Equal(tC.color, sut.color)
+		})
+	}
+}
+
+func TestFakeDeviceOff(t *testing.T) {
+	testCases := []struct {
+		desc             string
+		previousRGBColor RGBColor
+	}{
+		{
+			desc:             "already off",
+			previousRGBColor: OffColor,
+		},
+		{
+			desc:             "was white",
+			previousRGBColor: RGBColor{red: 255, green: 255, blue: 255},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			assert := assert.New(t)
+			sut := FakeDevice{}
+			sut.SetStaticColor(tC.previousRGBColor)
+
+			sut.Off()
+
+			assert.Equal(OffColor, sut.color)
 		})
 	}
 }
