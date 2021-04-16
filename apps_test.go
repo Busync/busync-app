338aee36bfcb68b3c4edce280ca278d2eca6205d
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-04-16 12:34:47 +0200

    test(apps): :white_check_mark: basic auth fake app

--- apps_test.go
+++ apps_test.go
@@ -21,36 +21,74 @@ func TestNotImplementedApp(t *testing.T) {
 	assert.EqualError(err, fmt.Sprintf("%s is not implemented", appName))
 }
 
+func mockHTTPRequestWithBasicAuth(mockedRequest *gock.Request, authConfig HTTPBasicAuthConfig) *gock.Request {
+	return mockedRequest.MatchHeader(
+		"Authorization",
+		"Basic "+basicAuth(authConfig.username, authConfig.password),
+	)
+}
+
 func TestFakeApp(t *testing.T) {
-	var testCases = []struct {
+	testCases := []struct {
 		desc           string
-		statusCode     int
-		json           FakeAppJSONResponse
 		httpClientType string
 		authConfig     interface{}
+		json           FakeAppJSONResponse
+		statusCode     int
 	}{
-		{desc: "is busy", statusCode: 200, json: FakeAppJSONResponse{IsBusy: true}, httpClientType: "no-auth", authConfig: nil},
-		{desc: "is not busy", statusCode: 200, json: FakeAppJSONResponse{IsBusy: false}, httpClientType: "no-auth", authConfig: nil},
+		{
+			desc:           "is busy without auth",
+			httpClientType: "no-auth",
+			authConfig:     nil,
+			json:           FakeAppJSONResponse{IsBusy: true},
+			statusCode:     200,
+		},
+		{
+			desc:           "is not busy without auth",
+			httpClientType: "no-auth",
+			authConfig:     nil,
+			json:           FakeAppJSONResponse{IsBusy: false},
+			statusCode:     200,
+		},
+		{
+			desc:           "is busy with basic auth",
+			httpClientType: "basic-auth",
+			authConfig:     HTTPBasicAuthConfig{username: "foobar", password: "spameggs"},
+			json:           FakeAppJSONResponse{IsBusy: true},
+			statusCode:     200,
+		},
+		{
+			desc:           "is not busy with basic auth",
+			httpClientType: "basic-auth",
+			authConfig:     HTTPBasicAuthConfig{username: "foobar", password: "spameggs"},
+			json:           FakeAppJSONResponse{IsBusy: false},
+			statusCode:     200,
+		},
 	}
-
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
 			assert := assert.New(t)
 			jsonMap := structs.Map(tC.json)
+
+			defer gock.Off()
+			mockedRequest := gock.New(FAKEAPP_API_URL).
+				Get(FAKEAPP_PATH)
+
+			switch tC.httpClientType {
+			case "basic-auth":
+				mockedRequest = mockHTTPRequestWithBasicAuth(mockedRequest, tC.authConfig.(HTTPBasicAuthConfig))
+			}
+
+			mockedRequest.Reply(tC.statusCode).
+				JSON(jsonMap)
+
 			httpClient, err := NewHTTPClient(tC.httpClientType, tC.authConfig)
 			assert.NoError(err)
-
-			gock.New(FAKEAPP_API_URL).
-				Get(FAKEAPP_PATH).
-				Reply(tC.statusCode).
-				JSON(jsonMap)
-			defer gock.Off()
 
 			sut, err := NewApp("fake", httpClient)
 			assert.NoError(err)
 
 			res, err := sut.isBusy()
-
 			assert.NoError(err, "isBusy() method should not raise an error")
 			assert.Equal(res, tC.json.IsBusy)
 		})
