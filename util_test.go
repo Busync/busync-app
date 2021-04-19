7978a70b976466b5b8f6477d304fdf2ab319f6f4
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-04-19 20:37:05 +0200

    test(util): add func to get name of a struct

--- util_test.go
+++ util_test.go
@@ -1,1 +1,13 @@
 package busylight_sync
+
+import (
+	"fmt"
+	"strings"
+)
+
+func GetStructName(s interface{}) string {
+	completeStructname := fmt.Sprintf("%T", s)
+	splittedStructName := strings.Split(completeStructname, ".")
+
+	return splittedStructName[len(splittedStructName)-1]
+}
