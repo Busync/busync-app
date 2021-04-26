932797a5533b9f3e73082ee3b21d489bf5762017
Kevin Marilleau <kevin.marilleau@gmail.com>
2021-04-26 14:44:50 +0200

    feat(main): load apps from given config

--- main.go
+++ main.go
@@ -14,3 +14,32 @@ func getHTTPClientFromAppConfig(config AppConfig) (*http.Client, error) {
 	return nil, errors.New("given app config is empty")
 }
 
+func getAppsFromGivenConfig(config *Config) ([]app, error) {
+	apps := make([]app, 0)
+
+	if config == nil {
+		return apps, errors.New("given config is nil")
+	}
+	
+	for appName, appConfig := range config.Apps {
+
+		httpClient, err := getHTTPClientFromAppConfig(appConfig)
+		if err != nil {
+			log.Printf("error when trying to load %s app http client: %s", appName, err)
+			continue
+		}
+
+		app, err := NewApp(appName, httpClient)
+		if err != nil {
+			log.Printf("error when trying to load %s app: %s", appName, err)
+		} else {
+			apps = append(apps, app)
+		}
+	}
+
+	if len(apps) == 0 {
+		return apps, errors.New("no app could be loaded from given config")
+	} else {
+		return apps, nil
+	}
+}
--- main_test.go
+++ main_test.go
@@ -7,6 +7,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func convertAppSliceToInterfaceSlice(apps []app) []interface{} {
+	var interfaceSlice []interface{} = make([]interface{}, len(apps))
+	for i, app := range apps {
+		interfaceSlice[i] = app
+	}
+
+	return interfaceSlice
+}
+
 func TestGetHTTPClientFromAppConfig(t *testing.T) {
 	testCases := []struct {
 		desc      string
@@ -39,6 +48,100 @@ func TestGetHTTPClientFromAppConfig(t *testing.T) {
 			} else {
 				assert.NoError(err)
 				assert.IsType(&http.Client{}, httpClient)
+			}
+		})
+	}
+}
+
+func TestGetAppsFromGivenConfig(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		config   *Config
+		wantApps []string
+		wantErr  string
+	}{
+		{
+			desc:    "config is nil",
+			wantErr: "given config is nil",
+		},
+		{
+			desc:    "no app",
+			config:  &Config{},
+			wantErr: "no app could be loaded from given config",
+		},
+		{
+			desc: "one app with no auth",
+			config: &Config{
+				Apps{
+					"toggl": {},
+				},
+			},
+			wantErr: "no app could be loaded from given config",
+		},
+		{
+			desc: "one non implemented app",
+			config: &Config{
+				Apps{
+					"foo": {
+						BasicAuth: HTTPBasicAuthConfig{
+							Username: "foobar",
+							Password: "spameggs",
+						},
+					},
+				},
+			},
+			wantErr: "no app could be loaded from given config",
+		},
+		{
+			desc: "one app",
+			config: &Config{
+				Apps{
+					"fake": {
+						BasicAuth: HTTPBasicAuthConfig{
+							Username: "foobar",
+							Password: "spameggs",
+						},
+					},
+				},
+			},
+			wantApps: []string{"FakeApp"},
+		},
+		{
+			desc: "two apps",
+			config: &Config{
+				Apps{
+					"fake": {
+						BasicAuth: HTTPBasicAuthConfig{
+							Username: "foobar",
+							Password: "spameggs",
+						},
+					},
+					"toggl": {
+						BasicAuth: HTTPBasicAuthConfig{
+							Username: "barbaz",
+							Password: "hamspam",
+						},
+					},
+				},
+			},
+			wantApps: []string{"FakeApp", "Toggl"},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			assert := assert.New(t)
+
+			apps, err := getAppsFromGivenConfig(tC.config)
+			appsInterfaceSlice := convertAppSliceToInterfaceSlice(apps)
+			appsNames := GetSliceOfStructNames(appsInterfaceSlice)
+
+			if err != nil {
+
+				assert.Empty(apps)
+				assert.EqualError(err, tC.wantErr)
+			} else {
+				assert.NoError(err)
+				assert.ElementsMatch(tC.wantApps, appsNames)
 			}
 		})
 	}
